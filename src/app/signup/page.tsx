02a479b63b92c347c104539ed7736f15200d4355
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-10-25 09:12:36 +0200

    Add signup form

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { Axios } from 'axios';
 
 function SignupPage() {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    username: '',
+  });
+
+  const onSignup = async () => {};
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Signup</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2 rounded-md border outline-none border-gray-300"
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 rounded-md border outline-none border-gray-300"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 rounded-md border outline-none border-gray-300"
+        id="password"
+        type="text"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
     </div>
   );
 }
