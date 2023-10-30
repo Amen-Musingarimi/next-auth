e710d2368923159eb51f9f019c9eb6637682c5be
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-10-30 22:23:05 +0200

    Style the sign up page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -15,36 +15,42 @@ function SignupPage() {
   const onSignup = async () => {};
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-4">
       <h1>Signup</h1>
       <hr />
-      <label htmlFor="username">username</label>
-      <input
-        className="p-2 rounded-md border outline-none border-gray-300"
-        id="username"
-        type="text"
-        value={user.username}
-        onChange={(e) => setUser({ ...user, username: e.target.value })}
-        placeholder="username"
-      />
-      <label htmlFor="email">email</label>
-      <input
-        className="p-2 rounded-md border outline-none border-gray-300"
-        id="email"
-        type="text"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
-        placeholder="email"
-      />
-      <label htmlFor="password">password</label>
-      <input
-        className="p-2 rounded-md border outline-none border-gray-300"
-        id="password"
-        type="password"
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
-        placeholder="password"
-      />
+      <div className="flex flex-col gap-1">
+        <label htmlFor="username">username</label>
+        <input
+          className="p-2 rounded-md border outline-none border-gray-300"
+          id="username"
+          type="text"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          placeholder="username"
+        />
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="email">email</label>
+        <input
+          className="p-2 rounded-md border outline-none border-gray-300"
+          id="email"
+          type="text"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="email"
+        />
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="password">password</label>
+        <input
+          className="p-2 rounded-md border outline-none border-gray-300"
+          id="password"
+          type="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="password"
+        />
+      </div>
       <button
         onClick={onSignup}
         className="p-2 border border-gray-300 rounded-md my-4 focus:outline-none focus:border-gray-600"
